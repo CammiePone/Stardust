1d8212fd6bd3a447145f011aebd585a93d3eeb3f
Cammie <inazuma261@gmail.com>
2019-01-10 20:41:28 -0500

    Reactor is complete

--- src/main/java/com/camellias/stardust/common/blocks/machines/generators/BlockMicroFissionReactor.java
+++ src/main/java/com/camellias/stardust/common/blocks/machines/generators/BlockMicroFissionReactor.java
@@ -1,5 +1,7 @@
 package com.camellias.stardust.common.blocks.machines.generators;
 
+import java.util.Random;
+
 import com.camellias.stardust.Main;
 import com.camellias.stardust.Reference;
 import com.camellias.stardust.common.tileentities.TileMicroFissionReactor;
@@ -19,8 +21,11 @@ import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockMicroFissionReactor extends Block implements IHasModel
 {
@@ -49,6 +54,22 @@ public class BlockMicroFissionReactor extends Block implements IHasModel
 		return true;
 	}
 	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void randomDisplayTick(IBlockState state, World world, BlockPos pos, Random rand)
+	{
+		TileMicroFissionReactor reactor = (TileMicroFissionReactor) world.getTileEntity(pos);
+		
+		if(reactor.isBurning())
+		{
+			double x = rand.nextGaussian() * 0.01D;
+            double z = rand.nextGaussian() * 0.01D;
+			
+			world.spawnParticle(EnumParticleTypes.EXPLOSION_NORMAL, pos.getX() + 0.5, pos.getY() + 1.25, pos.getZ() + 0.5, x, 0.02D, z);
+			world.spawnParticle(EnumParticleTypes.EXPLOSION_NORMAL, pos.getX() + 0.5, pos.getY() + 1.25, pos.getZ() + 0.5, x, 0.02D, z);
+		}
+	}
+	
 	@Override
 	public boolean hasTileEntity(IBlockState state)
 	{
@@ -64,7 +85,7 @@ public class BlockMicroFissionReactor extends Block implements IHasModel
 	@Override
 	public void breakBlock(World world, BlockPos pos, IBlockState state)
 	{
-		TileMicroFissionReactor reactor = (TileMicroFissionReactor)world.getTileEntity(pos);
+		TileMicroFissionReactor reactor = (TileMicroFissionReactor) world.getTileEntity(pos);
 		
 		world.spawnEntity(new EntityItem(world, pos.getX(), pos.getY(), pos.getZ(), reactor.handler.getStackInSlot(0)));
 		
@@ -90,13 +111,13 @@ public class BlockMicroFissionReactor extends Block implements IHasModel
 	@Override
 	public boolean isTranslucent(IBlockState state)
 	{
-		return true;
+		return false;
 	}
 	
 	@Override
 	public BlockRenderLayer getBlockLayer()
 	{
-		return BlockRenderLayer.TRANSLUCENT;
+		return BlockRenderLayer.SOLID;
 	}
 	
 	@Override
